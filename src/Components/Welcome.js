44c4d4f35146f37f541481556989cafc2a93b55d
mahnooriqbal212 <142347782+mahnooriqbal212@users.noreply.github.com>
2023-08-16 14:18:24 +0500

    Update Welcome.js

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -48,7 +48,7 @@ const Welcome = ({ user, setUser }) => {
       if (res) {
         // perform axios reqeust
         const resp = await axios.post(
-          "https://a682-34-125-15-23.ngrok-free.app/" +
+          "https://ea5a-35-201-251-216.ngrok-free.app/" +
             "pslinterpretor/backend",
           {
             url: res.Location,
